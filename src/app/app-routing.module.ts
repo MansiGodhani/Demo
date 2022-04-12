a146cacd8aa34a6da2a1cd778e808f56a8703c43
Mansi <godhanimansi71@gmail.com>
2022-04-12 16:51:41 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {EmployeeComponent} from "./components/employee/employee.component";
+import {AddEmployeeComponent} from "./components/add-employee/add-employee.component";
+import {EditEmployeeComponent} from "./components/edit-employee/edit-employee.component";
+
+const routes: Routes = [
+  { path:'', component:EmployeeComponent},
+  { path:'addEmployees', component:AddEmployeeComponent},
+  { path:'employees/edit/:id', component:EditEmployeeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import {AddEmployeeComponent} from "./components/add-employee/add-employee.component";
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
